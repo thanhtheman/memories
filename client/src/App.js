e44a781458223490d10edf2bed3fa2e4d0d0ab08
thanh quach <thanhb.quach@gmail.com>
2022-07-18 11:10:36 -0400

    debug setCurrentId

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import memories from './images/memories.png';
@@ -8,36 +8,34 @@ import useStyles from './styles';
 import { getPosts } from './actions/posts';
 
 const App = () => {
-
-  const classess = useStyles();
+  const [currentId, setCurrentId] = useState(0);
+  const classes = useStyles();
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  });
 
   // useEffect(() => {
   //   dispatch(getPosts());
-  // }, [currentId, dispatch]);
+  // });
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
 
 
   return (
-    <Container maxWidth='lg'>
-      <AppBar className={classess.appBar} position='static' color='inherit'>
-        <Typography className={classess.heading} variant='h2' align='center'>
-          Private Memories
-        </Typography>
-        <img className={classess.img} src={memories} alt="memories" height='60' />
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify='space-between' alignItems='stretch' spacing={3}>
-              <Grid item xs={12} sm={7}>
-                <Posts />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <Form />
-              </Grid>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
           </Grid>
         </Container>
       </Grow>
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -20,7 +20,7 @@ const Form = ({ currentId, setCurrentId }) => {
   useEffect(() => {
     if (post) setPostData(post);
   }, [post]);
-  
+
   const clear = () => {
     setCurrentId(0);
     setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
@@ -42,7 +42,7 @@ const Form = ({ currentId, setCurrentId }) => {
   return (
     <Paper className={classes.paper}>
           <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-          <Typography variant='h6'>Creating a Memory</Typography>
+          <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
           <TextField name='creator' variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
           <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
           <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
