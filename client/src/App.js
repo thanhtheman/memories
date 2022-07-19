2878fa697c9c49a49ac6b273bc44bd6236fc3243
thanh quach <thanhb.quach@gmail.com>
2022-07-19 17:31:35 -0400

    pagination and refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 import useStyles from './styles';
+import PostDetails from './components/PostDetails/PostDetails';
 
 const App = () => {
   const classes = useStyles();
+  const user = JSON.parse(localStorage.getItem('profile'));
+
   return (
     <BrowserRouter className={classes.appBar}>
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth/>} />
+          <Route path="/" exact element={<Navigate to="/posts" />}/>
+          <Route path="/posts" exact element={<Home />}/>
+          <Route path="/posts/search" exact element={<Home />}/>
+          <Route path="/posts/:id" exact element={<PostDetails />}/>
+          <Route path="/auth" exact element={!user ? <Auth /> : <Navigate to="/posts"/>} />
         </Routes>
       </Container>
     </BrowserRouter>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import useStyles from './styles';
 import { getPosts } from '../../actions/posts';
 import { useDispatch } from 'react-redux';
+import Pagination from '../Pagination/Pagination';
+import { useNavigate, useLocation } from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input';
 
 const Home = () => {
 
   const [currentId, setCurrentId] = useState(0);
   const classes = useStyles();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
 
 
   useEffect(() => {
@@ -25,6 +30,9 @@ const Home = () => {
             </Grid>
             <Grid item xs={12} sm={4}>
               <Form currentId={currentId} setCurrentId={setCurrentId} />
+              <Paper elevation={6}>
+                <Pagination />
+              </Paper>
             </Grid>
           </Grid>
         </Container>
